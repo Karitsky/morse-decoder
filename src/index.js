59b20ffb412bd0906093bef99f035620cd735c61
karitsky <valery.karitsky@gmail.com>
2019-09-28 01:23:50 +0300

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,36 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let result = '';
+
+    for (let i = 0; i < expr.length; i += 10) {
+        let singleLetter = expr.substr(i, 10);
+
+        if (singleLetter == '**********') {
+            result += ' ';
+        } else {
+            
+            let singleLetterMorse = '';
+
+            for (let j = 0; j < singleLetter.length; j += 2) {
+                let singleChar = singleLetter.substr(j, 2);
+                if (singleChar == '10') {
+                    singleCharMorse = '.';
+                } else if (singleChar == '11') {
+                    singleCharMorse = '-';
+                } else {
+                    continue;
+                }
+                singleLetterMorse += singleCharMorse;
+            }
+
+            result += MORSE_TABLE[singleLetterMorse];
+
+        }
+        
+    }
+    return result;
 }
 
 module.exports = {
